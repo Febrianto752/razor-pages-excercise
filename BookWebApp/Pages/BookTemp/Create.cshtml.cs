b795a6f744863518dcf5d5f92841372b127e64c4
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-06 15:12:01 +0700

    feat: add BookTemp

---
+++ BookWebApp/Pages/BookTemp/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BookWebApp.Data;
+using BookWebApp.Models;
+
+namespace BookWebApp.Pages.BookTemp
+{
+    public class CreateModel : PageModel
+    {
+        private readonly BookWebApp.Data.BookDbContext _context;
+
+        public CreateModel(BookWebApp.Data.BookDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Books == null || Book == null)
+            {
+                return Page();
+            }
+
+            _context.Books.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
