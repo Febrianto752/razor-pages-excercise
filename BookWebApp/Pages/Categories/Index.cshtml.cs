59b9a7ad928e30828e5ed02f1713a72a51758186
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-06 10:54:26 +0700

    feat: add book list

---
+++ BookWebApp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using BookWebApp.Data;
+using BookWebApp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookWebApp.Pages.Categories;
+
+
+public class IndexModel : PageModel
+{
+    private readonly BookDbContext _db;
+    public IEnumerable<Book> Books { get; set; }
+
+    public IndexModel(BookDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet()
+    {
+        Books = _db.Books;
+    }
+}
+
