dbefbfc5ecfe06dd0d259f000c08b502a02d2ebc
Febrianto752 <febrianto.bekasi@gmail.com>
2023-09-06 09:56:28 +0700

    intiial commit - add migration

---
+++ BookWebApp/Data/BookDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWebApp.Data;
+
+public class BookDbContext : DbContext
+{
+    public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+}
+
---
+++ BookWebApp/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookWebApp.Models;
+
+public class Book
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+
+    [DefaultValue(0)]
+    public int Quantity { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdateDate { get; set; }
+}
+
